f9e3ff48519b377bf91e5de0fb0c88dc8174b98d
Zubair <91695367+zubxxr@users.noreply.github.com>
2025-06-06 03:12:29 -0400

    Update concatenate_and_time_sync_nodelet.cpp

--- sensing/autoware_pointcloud_preprocessor/src/concatenate_data/concatenate_and_time_sync_nodelet.cpp
+++ sensing/autoware_pointcloud_preprocessor/src/concatenate_data/concatenate_and_time_sync_nodelet.cpp
@@ -437,9 +437,10 @@ void PointCloudConcatenateDataSynchronizerComponent::publish()
         auto output = std::make_unique<sensor_msgs::msg::PointCloud2>(*e.second);
         transformed_raw_pc_publisher_map_[e.first]->publish(std::move(output));
       } else {
-        RCLCPP_WARN(
-          this->get_logger(), "transformed_raw_points[%s] is nullptr, skipping pointcloud publish.",
-          e.first.c_str());
+        (void)e.first.c_str();
+        //RCLCPP_WARN(
+          //this->get_logger(), "transformed_raw_points[%s] is nullptr, skipping pointcloud publish.",
+         // e.first.c_str());
       }
     }
   }
